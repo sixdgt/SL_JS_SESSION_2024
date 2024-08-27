f59dcdeb87679600dd5681bc0ecfa79954d526f2
sandesh <c4crypt@gmaill.com>
2024-08-27 12:04:18 +0545

    Day 11 - DOM Styling Element

---
+++ day_eleven/script.js
@@ -0,0 +1,13 @@
+let headingTwo = document.getElementById("headingTwo");
+
+headingTwo.style.color = "red";
+headingTwo.style.fontSize= "2rem";
+headingTwo.style.fontFamily = "open-sans";
+headingTwo.style.textAlign = "center";
+headingTwo.style.textDecoration = "underline";
+headingTwo.style.textTransform = "uppercase";
+
+let content = document.getElementById("content");
+content.style.textTransform = "lowercase";
+content.style.textAlign = "center";
+content.style.color = "orange";
